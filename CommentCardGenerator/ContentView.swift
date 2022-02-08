178f927c64d626b98cf3fc37ddcbe247e2c155f6
Ssnehnil Ghosh <ghosh.s@etoncollege.org.uk>
2022-02-08 11:20:32 +0000

    First MVP

--- CommentCardGenerator/ContentView.swift
+++ CommentCardGenerator/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isFeeling: Bool
+    @State var isStrenuous: Bool
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            VStack {
+                if isFeeling == true {
+                    Button("Feeling: 😁", action: { toggleFeeling()})
+                } else {
+                    Button("Feeling: ☹️", action: { toggleFeeling()})
+                }
+            }
+            Text("I am feeling \(isFeeling ? "happy" : "unhappy") in this subject.")
+            VStack {
+                if isStrenuous == true {
+                    Button("Time: 😁", action: { toggleFeeling()})
+                } else {
+                    Button("Feeling: ☹️", action: { toggleFeeling()})
+                }
+            }
+        }
+    }
+    
+    func toggleFeeling() {
+        isFeeling.toggle()
+    }
+    
+    func toggleStrenuous() {
+        isStrenuous.toggle()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(isFeeling: true, isStrenuous: true)
     }
 }
