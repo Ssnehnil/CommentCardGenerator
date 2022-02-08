8d3ca7c5b7f829ee2c146617ac62451ab157a035
Ssnehnil Ghosh <ghosh.s@etoncollege.org.uk>
2022-02-08 10:52:35 +0000

    Initial Commit

---
+++ CommentCardGenerator/CommentCardGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommentCardGeneratorApp.swift
+//  CommentCardGenerator
+//
+//  Created by Ghosh, Ssnehnil (BJH) on 08/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CommentCardGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
