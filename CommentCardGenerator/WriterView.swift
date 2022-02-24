488969c2c3af476cdd61d73ad5316513b9a549fb
Ssnehnil <ghosh.s@etoncollege.org.uk>
2022-02-24 07:52:20 +0000

    Finished writer section.

--- CommentCardGenerator/WriterView.swift
+++ CommentCardGenerator/WriterView.swift
@@ -11,6 +11,9 @@ struct ContentView: View {
     @State private var strenuous: Double = 5.0
     @State private var happiness: Double = 5.0
     @State private var lotsEW = true
+    @State private var progress: Double = 5.0
+    @State private var effort: Double = 5.0
+    @State private var achievement: Double = 5.0
     
     var feelingHappiness: String {
         if happiness > 8 {
@@ -28,7 +31,7 @@ struct ContentView: View {
         if strenuous > 8 {
             return "too long a"
         } else if strenuous > 6 {
-            return "occaisionally a little too much"
+            return "occasionally a little too much"
         } else if strenuous > 3 {
             return "the perfect amount of"
         } else {
@@ -41,6 +44,42 @@ struct ContentView: View {
             return "I find that there is too much EW being set."
         } else {
             return "I think that the amount of EW set is fine."
+        }
+    }
+    
+    var measureProgress: String {
+        if progress > 8 {
+            return "extremely pleased"
+        } else if progress > 6 {
+            return "satisfied"
+        } else if progress > 3 {
+            return "dissatisfied"
+        } else {
+            return "very disappointed"
+        }
+    }
+    
+    var measureEffort: String {
+        if effort > 8 {
+            return "a lot of"
+        } else if effort > 5 {
+            return "a good amount of"
+        } else if effort > 2 {
+            return "not enough"
+        } else {
+            return "almost zero"
+        }
+    }
+    
+    var measureAchievement: String {
+        if achievement > 8 {
+            return "high"
+        } else if achievement > 6 {
+            return "good"
+        } else if achievement > 3 {
+            return "okay but can definately be improved"
+        } else {
+            return "below average and should be better"
         }
     }
     
@@ -57,10 +96,25 @@ struct ContentView: View {
                 Text("10")
             }.padding()
             HStack {
+                Text("Progress: 0")
+                Slider(value: $progress, in: 0...10, step: 1)
+                Text("10")
+            }.padding()
+            HStack {
+                Text("Effort: 0")
+                Slider(value: $effort, in: 0...10, step: 1)
+                Text("10")
+            }.padding()
+            HStack {
+                Text("Achievement: 0")
+                Slider(value: $achievement, in: 0...10, step: 1)
+                Text("10")
+            }.padding()
+            HStack {
                 Toggle("Too many EWs", isOn: $lotsEW)
             }.padding()
             Spacer()
-            Text("I am feeling \(feelingHappiness) in this subject and I spend \(feelingStrenuous) time on this subject. \(homework)").padding()
+            Text("I am feeling \(feelingHappiness) in this subject and I spend \(feelingStrenuous) time on this subject. \(homework) So far, I feel \(measureProgress) with my level of progress. Furthermore, I am putting \(measureEffort) effort into this subject, moreover in my opinion my achievement levels are \(measureAchievement).").padding()
             Spacer()
         }
         
